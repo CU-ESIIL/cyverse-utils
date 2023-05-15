77e0334ace75fd9d1af7d17e53d9126014232f97
Erick Verleye <erve3705@colorado.edu>
2023-05-15 15:40:21 -0600

    Just using subprocess to kubectl and converting ssh config to ipynb

--- kubernetes_api.py
+++ kubernetes_api.py
@@ -1,7 +1,5 @@
-import os
-import shutil
 from kubernetes import client, config
-from kubernetes.stream import stream
+import subprocess as sp
 
 
 class KubernetesAPI:
@@ -26,23 +24,7 @@ class KubernetesAPI:
         user_pods = self.get_user_pods()
         for pod in user_pods:
             # Copy the file to the pod
-            command = ['cp', source_path, f'{pod.metadata.name}:{dest_path}']
+            command = ['kubectl', 'cp', source_path, f'{pod.metadata.name}:{dest_path}']
             if recursive:
                 command.insert(1, '-r')
-
-            resp = stream(
-                self.v1.connect_get_namespaced_pod_exec,
-                pod.metadata.name,
-                pod.metadata.namespace,
-                command=command,
-                stderr=True,
-                stdin=False,
-                stdout=True,
-                tty=False,
-            )
-
-            # Check if the file was copied successfully
-            # if resp.exit_code == 0:
-            #     print(f"File copied to {pod.metadata.name}")
-            # else:
-            #     print(f"Failed to copy file to {pod.metadata.name}")
+            sp.call(command)
