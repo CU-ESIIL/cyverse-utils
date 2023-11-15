20058b043f13cd7b7445b40786d6a60ea47f36af
Erick Verleye <erve3705@colorado.edu>
2023-11-15 16:32:06 -0500

    Adding R file for github ssh

---
+++ configure_github_ssh.R
@@ -0,0 +1,57 @@
+# Install the processx package if not already installed
+if (!requireNamespace("processx", quietly = TRUE)) {
+  install.packages("processx")
+}
+library(processx)
+
+# Function to add GitHub to known hosts
+add_github_to_known_hosts <- function() {
+  known_hosts_file <- path.expand("~/.ssh/known_hosts")
+  host <- "github.com"
+
+  # Retrieve the host key using ssh-keyscan
+  host_key <- system2("ssh-keyscan", args = c("-t", "rsa", host), stdout = TRUE)
+
+  # Append the host key to the known hosts file
+  cat(host_key, file = known_hosts_file, append = TRUE)
+
+  cat(paste0(host, " added to known hosts file.\n"))
+}
+
+# Main configuration function
+configure <- function() {
+  username <- readline(prompt = "GitHub username: ")
+  email <- readline(prompt = "GitHub email: ")
+
+  system2("git", args = c("config", "--global", "user.name", username))
+  system2("git", args = c("config", "--global", "user.email", email))
+
+  # Generate Ed25519 keypair
+  ssh_dir <- path.expand("~/.ssh")
+  key_name <- "github"
+  private_key_path <- file.path(ssh_dir, key_name)
+  public_key_path <- paste0(private_key_path, ".pub")
+
+  system2("ssh-keygen", args = c("-t", "ed25519", "-f", private_key_path, "-N", ""))
+
+  # Update SSH config
+  config_file <- file.path(ssh_dir, "config")
+  github_config <- sprintf("
+Host github.com
+    HostName github.com
+    IdentityFile %s
+", private_key_path)
+
+  cat(github_config, file = config_file, append = TRUE)
+
+  # Print public key
+  public_key <- readLines(public_key_path)
+  cat("Public Key:\n")
+  cat(public_key, sep = "\n")
+
+  # Add GitHub to known hosts
+  add_github_to_known_hosts()
+}
+
+# Run the configuration
+configure()
